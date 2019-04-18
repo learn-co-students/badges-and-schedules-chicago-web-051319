0919261c87e9a5e8e25cd2e340b7d5b2da43c3e7
Emily R <emilyroulund@gmail.com>
2019-04-18 21:59:16 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker (name)
+  i = 0
+  while i < name.length 
+  return "Hello, my name is #{name[i}."
+  i+=1
+end
+end
+
+def batch_badge_creator (array_names)
+  array_names = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+  return badge_maker (array_names) 
+end 
+
+def assign_rooms (array_names)
+  return "Hello, _____! You'll be assigned to room _____!"
+end 
+
+def printer 
+  return batch_badge_creator (array_names) 
+  return assign_rooms (array_names)
+end 
